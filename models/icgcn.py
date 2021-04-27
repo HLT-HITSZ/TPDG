429f9bfe69b2639a6005a013592a1c2d1827cc4a
neptune1997 <18040852925@163.com>
2021-04-27 11:02:35 +0800

    refine code and delete extra code

--- models/icgcn.py
+++ models/icgcn.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -60,39 +59,6 @@ class ICGCN(nn.Module):
 
         self.text_embed_dropout = nn.Dropout(0.3)
 
-    def position_weight(self, x, aspect_double_idx, text_len, aspect_len):
-        batch_size = x.shape[0]
-        seq_len = x.shape[1]
-        aspect_double_idx = aspect_double_idx.cpu().numpy()
-        text_len = text_len.cpu().numpy()
-        aspect_len = aspect_len.cpu().numpy()
-        weight = [[] for i in range(batch_size)]
-        for i in range(batch_size):
-            context_len = text_len[i] - aspect_len[i]
-            for j in range(aspect_double_idx[i,0]):
-                weight[i].append(1-(aspect_double_idx[i,0]-j)/context_len)
-            for j in range(aspect_double_idx[i,0], aspect_double_idx[i,1]+1):
-                weight[i].append(0)
-            for j in range(aspect_double_idx[i,1]+1, text_len[i]):
-                weight[i].append(1-(j-aspect_double_idx[i,1])/context_len)
-            for j in range(text_len[i], seq_len):
-                weight[i].append(0)
-        weight = torch.tensor(weight).unsqueeze(2).to(self.opt.device)
-        return weight*x
-
-    def mask(self, x, aspect_double_idx):
-        batch_size, seq_len = x.shape[0], x.shape[1]
-        aspect_double_idx = aspect_double_idx.cpu().numpy()
-        mask = [[] for i in range(batch_size)]
-        for i in range(batch_size):
-            for j in range(aspect_double_idx[i,0]):
-                mask[i].append(0)
-            for j in range(aspect_double_idx[i,0], aspect_double_idx[i,1]+1):
-                mask[i].append(1)
-            for j in range(aspect_double_idx[i,1]+1, seq_len):
-                mask[i].append(0)
-        mask = torch.tensor(mask).unsqueeze(2).float().to(self.opt.device)
-        return mask*x
 
     def forward(self, inputs):
         text_indices, target_indices, in_adj, cross_adj = inputs
@@ -158,39 +124,6 @@ class ICGCNBert(BertPreTrainedModel):
         self.text_embed_dropout = nn.Dropout(0.3)
         self.init_weights()
 
-    def position_weight(self, x, aspect_double_idx, text_len, aspect_len):
-        batch_size = x.shape[0]
-        seq_len = x.shape[1]
-        aspect_double_idx = aspect_double_idx.cpu().numpy()
-        text_len = text_len.cpu().numpy()
-        aspect_len = aspect_len.cpu().numpy()
-        weight = [[] for i in range(batch_size)]
-        for i in range(batch_size):
-            context_len = text_len[i] - aspect_len[i]
-            for j in range(aspect_double_idx[i,0]):
-                weight[i].append(1-(aspect_double_idx[i,0]-j)/context_len)
-            for j in range(aspect_double_idx[i,0], aspect_double_idx[i,1]+1):
-                weight[i].append(0)
-            for j in range(aspect_double_idx[i,1]+1, text_len[i]):
-                weight[i].append(1-(j-aspect_double_idx[i,1])/context_len)
-            for j in range(text_len[i], seq_len):
-                weight[i].append(0)
-        weight = torch.tensor(weight).unsqueeze(2).to(self.opt.device)
-        return weight*x
-
-    def mask(self, x, aspect_double_idx):
-        batch_size, seq_len = x.shape[0], x.shape[1]
-        aspect_double_idx = aspect_double_idx.cpu().numpy()
-        mask = [[] for i in range(batch_size)]
-        for i in range(batch_size):
-            for j in range(aspect_double_idx[i,0]):
-                mask[i].append(0)
-            for j in range(aspect_double_idx[i,0], aspect_double_idx[i,1]+1):
-                mask[i].append(1)
-            for j in range(aspect_double_idx[i,1]+1, seq_len):
-                mask[i].append(0)
-        mask = torch.tensor(mask).unsqueeze(2).float().to(self.opt.device)
-        return mask*x
 
     def forward(self, inputs):
         text_indices, attention_mask, in_adj, cross_adj = inputs
@@ -234,8 +167,8 @@ class ICGCNBert(BertPreTrainedModel):
 
 if __name__=="__main__":
     from transformers import BertConfig
-    config = BertConfig.from_pretrained("/home/fuyonghao/pretrained_models/bert_base_en")
+    config = BertConfig.from_pretrained("path_to_pretrain_model/pretrained_models/bert_base_en")
     config.gcn_layers = 3
     config.polarities_size = 3
-    model = ICGCNBert.from_pretrained("/home/fuyonghao/pretrained_models/bert_base_en",config=config)
+    model = ICGCNBert.from_pretrained("path_to_pretrain_model/pretrained_models/bert_base_en",config=config)
     named_parameters = list(model.named_parameters())
